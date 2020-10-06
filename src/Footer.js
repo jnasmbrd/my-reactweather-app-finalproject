afac8ac1324297ef0a3c5374624de218ca018bd6
Janice Asmbrd <jnasmbrd@gmail.com>
2020-10-06 14:36:19 -0600

    Added link to the footer and updated css.

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 import './Footer.css';
 
-export default function (){
-    return (<div><footer>Open Source Code by Janice Asumbrado</footer></div>);
+export default function Footer (){
+    return (
+    <div>
+        <footer>
+            <a href="https://github.com/jnasmbrd/my-reactweather-app-finalproject.git" target="blank"> Open Source Code</a> {""} by {""}  
+            <a href="">Janice Asumbrado</a>.
+            </footer>
+            </div>);
 }
